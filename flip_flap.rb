537d6e140e74e62d6d65768d51e7f0aa29d6b326
HW-Lee <tony123930@gmail.com>
2015-10-01 17:37:38 +0800

    the task has been done.

--- flip_flap.rb
+++ flip_flap.rb
@@ -1,6 +1,16 @@
-require_relative 'tsv_buddy'
+require_relative './tsv_buddy'
+require 'yaml'
 
 class FlipFlap
   # Do NOT create an initialize method
+  include TsvBuddy
 
+  def take_yaml(yaml)
+    @data = YAML.load(yaml)
+    self
+  end
+
+  def to_yaml
+    @data.to_yaml
+  end
 end
--- tsv_buddy.rb
+++ tsv_buddy.rb
@@ -7,12 +7,20 @@ module TsvBuddy
   # take_tsv: converts a String with TSV data into @data
   # parameter: tsv - a String in TSV format
   def take_tsv(tsv)
-
+    lines = tsv.split "\n"
+    if lines.length > 0
+      keys = lines[0].split "\t"
+      values = lines[1..-1].map { |x| x.split "\t" }
+      @data = values.map { |value| Hash[ keys.zip( value ) ] }
+      self
+    end
   end
 
   # to_tsv: converts @data into tsv string
   # returns: String in TSV format
   def to_tsv
-
+    keys = @data[0].keys
+    values = @data.map { |x| x.values.reduce { |x, y| "#{x}\t#{y}" } }
+    keys.join( "\t" ) + "\n" + values.join( "\n" ) + "\n"
   end
 end
